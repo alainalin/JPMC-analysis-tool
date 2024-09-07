eba792dbfa5f8cc20e603ff553882922a00081b1
alainalin <alaina_lin@brown.edu>
2024-09-07 14:05:31 -0400

    add pandas_datareader package to env

--- preprocess.py
+++ preprocess.py
@@ -1,9 +1,8 @@
 import os
 import json
 
-import yfinance as yf
-import pandas_datareader.data as web; yf.pdr_override()
 import pandas as pd
 import numpy as np
+import yfinance as yf
+import pandas_datareader.data as web; yf.pdr_override()
 
-import matplotlib.pyplot as plt
